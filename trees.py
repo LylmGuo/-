71a5957e7deb543bdce1a8ca1563e8a01598ce3c
LylmGuo <39119871+LylmGuo@users.noreply.github.com>
2019-11-04 12:00:10 +0800

    Update trees.py

--- trees.py
+++ trees.py
@@ -34,3 +34,50 @@ calcShannonEnt()步骤：
 """
 
   
+def splitDataSet(dataSet,axis,value):  #待划分的数据集，用于划分数据集的特征列下标，我们想返回特征的值
+    retDataSet = []
+    for featVec in dataSet: 
+        if featVec[axis] == value: #featVec [1, 1, 'yes'] axis=0：如果这一行第一列的值和value一致，进行以下操作
+            reducedFeatVec = featVec[:axis] #获取这行第0到第axis-1列的列表
+            reducedFeatVec.extend(featVec[axis+1:]) #reducedFeatVec加上featVec后面的列
+            retDataSet.append(reducedFeatVec) #然后放到retDataSet列表中
+    return retDataSet #返回的是除去（用来做划分数据集的特征）的数据集
+
+"""
+splitDataSet()步骤：
+1. 循环查看数据集，选用于划分数据集的特征列，对比其值是否与我们想返回的特征的值相同
+2. 若相同，删除该特征列，保留剩余的特征列和分类列（即“yes”和“no”那一列）
+3. 存入新的数据集中，循环结束后返回新的数据集
+
+"""
+def chooseBestFeatureToSplit(dataSet): #dataSet每一行的最后一列必须为类型
+    numFeatures = len(dataSet[0]) - 1  #计算减去类型后共有多少个特征
+    baseEntropy = calcShannonEnt(dataSet) #计算原有的dataSet的熵
+    bestInfoGain = 0.0; bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet] #把第i列的数据提取出来，放到一个list中
+        uniqueVals = set(featList) #去重
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet,i,value) #循环用去重后的每一个结果分别来划分dataSet（i与每个value匹配；与value相同的作为子集返回）
+            prob = len(subDataSet)/float(len(dataSet)) #计算子集出现概率
+            newEntropy += prob * calcShannonEnt(subDataSet) #计算熵
+        infoGain = baseEntropy - newEntropy #计算信息增益，即旧熵-新熵
+        if infoGain > bestInfoGain: #信息增益比较
+            bestInfoGain = infoGain
+            bestFeature = i #若满足条件 最好的特征就是第i个特征
+    return bestFeature
+#循环，遍历特征；遍历到某特征后，假设按其划分，要循环计算子集中不同种类（即不同value）的信息熵和信息增益；综上，来确定哪个特征来分类最好
+"""
+chooseBestFeatureToSplit()步骤：
+1. 计算有多少个特征（dataSet中除去最后一列都是特征列）
+2. 通过calcShannonEnt()函数计算dataSet的熵
+3. 设初始最好信息增益为0，特征列下标为-1
+4. 循环numFeatures（特征列数）次，将该列的数据提取出来并去重，存为uniqueVals
+5. 循环uniqueVals的值，以其作为区分值，传给splitDataSet()进行数据集划分，存为子集
+6. 计算子集出现概率
+7. 计算子集的熵
+8. 计算信息增益（旧熵-新熵）
+9. 比较信息增益和最好信息增益，若本次信息增益较大，则存为最好信息增益，该特征存为最优特征
+10. 循环结束后，返回最优特征
+"""
